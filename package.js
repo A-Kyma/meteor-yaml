1f8142b8a76288c190f83799c6be593c968e3796
arnaud <adoyen@perso.be>
2024-08-18 11:09:08 +0200

    Update for compatibility of Meteor 3.0 - publication

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: 'slaivyn:yaml',
-  summary: 'Import/require YAML 1.2 files as JS objects in Meteor project',
-  version: '1.0.1',
+  name: 'akyma:yaml',
+  summary: 'Import/require YAML 1.2 files as JS objects in Meteor v3 project',
+  version: '2.0.1',
 })
 
 Package.registerBuildPlugin({
   name: 'compile-yaml',
-  use: ['caching-compiler@1.2.1', 'ecmascript@0.12.4'],
+  use: ['caching-compiler@2.0.0', 'ecmascript@0.16.8'],
   sources: ['compile-yaml.js'],
   npmDependencies: {
-    'js-yaml': '3.12.2'
+    'js-yaml': '4.1.0'
   },
 })
 
 Package.onUse(function (api) {
-  api.use('isobuild:compiler-plugin@1.0.0')
+  api.use('isobuild:compiler-plugin@1.0.0');
 })
